f60525251db43760196339d9f19c88631232af95
kyajpauley <kyajmil@gmail.com>
2018-11-07 12:49:50 +0900

    add error output

--- examples/test_stanfordnlp_wrapper.go
+++ examples/test_stanfordnlp_wrapper.go
@@ -9,16 +9,22 @@ func main() {
 	var (
 		tagger *pos_tagger.Tagger
 		res    []*pos_tagger.Result
-		//err error
+		err    error
 	)
 
 	path := "/home/kya/Documents/stanford-postagger-full-2018-10-16/"
 	modelPath := fmt.Sprintf("%s/models/chinese-distsim.tagger", path)
 	taggerPath := fmt.Sprintf("%s/stanford-postagger.jar", path)
 
-	tagger, _ = pos_tagger.NewTagger(modelPath, taggerPath)
+	tagger, err = pos_tagger.NewTagger(modelPath, taggerPath)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	res, _ = tagger.Tag("我来 到 北京 清华大学")
+	res, err = tagger.Tag("我来 到 北京 清华大学")
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	for _, r := range res {
 		fmt.Println(r.Word, r.TAG)
